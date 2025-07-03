f2230c5dca4a642cbcfd5a98506eb5a43caebe67
clark <clark.hobbie@gmail.com>
2025-07-03 08:19:13 -0600

    Added some javadoc

--- src/main/java/com/ltsllc/password/ImprovedPaths.java
+++ src/main/java/com/ltsllc/password/ImprovedPaths.java
@@ -4,18 +4,43 @@ import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+/******************************************************************************
+ * Provide some convenience methods for working with {@link java.nio.file.Path}s
+ */
 public class ImprovedPaths {
+    /**************************************************************************
+     * Create a {@link java.nio.file.Path} from a file name.
+     *
+     * @param fileName The file name.
+     * @return The corresponding {@link java.nio.file.Path}
+     */
     public static Path toPath(String fileName) {
         File file = new File(fileName);
         return toPath(file);
-
     }
+
+    /**************************************************************************
+     * Create a {@link java.nio.file.Path} from a {@link java.io.File}.
+     *
+     * @param file The {@link java.io.File}.
+     * @return The corresponding {@link java.nio.file.Path}.
+     */
     public static Path toPath(File file) {
         String pathString = file.getAbsolutePath();
         Path path = Paths.get(pathString);
         return path;
     }
 
+    /**************************************************************************
+     * Return the new {@link java.nio.file.Path} created from appending a
+     *
+     * Return a {@link java.nio.file.Path} resulting from appending a string to
+     * a {@link java.nio.file.Path}.
+     *
+     * @param path The {@link java.nio.file.Path} to append to.
+     * @param suffix The string to append.
+     * @return The resulting {@link java.nio.file.Path}.
+     */
     public static Path appendToPath(Path path, String suffix) {
         String string = path + suffix;
         Path tempPath = Path.of (string);
--- src/main/java/com/ltsllc/password/Password.java
+++ src/main/java/com/ltsllc/password/Password.java
@@ -2,10 +2,25 @@ package com.ltsllc.password;
 
 import java.io.File;
 
+/******************************************************************************
+ * Create a new, random password  subject to some constraints.
+ */
 public class Password {
     public static PasswordProperties properties = new PasswordProperties();
     public static final String DEFAULT_PROPERTIES_FILE = "password.properties";
 
+
+    /**************************************************************************
+     * Generate a new password.
+     *
+     * The method uses the properties from
+     * {@link PasswordProperties#DEFAULT_PROPERTIES_FILE_NAME}, or it creates a
+     * new file by that name and gives it the properties defined by
+     * {@link PasswordProperties#setDefaultProperties()}.
+     *
+     * @see {@link PasswordProperties#setDefaultProperties()}
+     * @return The generated password.
+     */
     public String generate () {
         File file = PasswordProperties.propertiesFile;
         if (!file.exists()) {
@@ -16,6 +31,17 @@ public class Password {
         return generatePassword();
     }
 
+    /**************************************************************************
+     * Generate a new password.
+     *
+     * The method parses in the list of requirements given by
+     * {@link PasswordProperties#getCandidatesString()} and adds
+     * {@link AnyLowerCaseCharacter} subject to the
+     * length given by {@link PasswordProperties#getLength()} and generates a
+     * new, random password.
+     *
+     * @return The new password.
+     */
     public String generatePassword() {
         Candidate[] candidates = Candidate.parse(properties.getCandidatesString());
         StringBuilder password = new StringBuilder();
